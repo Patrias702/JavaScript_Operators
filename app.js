310d45aaa905ce1d76e97cd7827ec989b68767de
DJ Patrias <Patrias21@gmail.com>
2023-03-31 19:39:33 -0700

    Done

--- app.js
+++ app.js
@@ -5,15 +5,59 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
+ let a = 20;
+ let b = 4;
+ let sum = a + b;
+ let minus = a - b ;
+ let multiply = a * b;
+ let dividing = a / b;
+ let remainder = a % b;
 
-// YOUR CODE HERE
+ console.log(20);
+ comnsole.log(4);
+ console.log(a + b);
+ console.log(a - b);
+ console.log (a * b);
+ console.log ( a / b);
+ console.log ( a % b);
+
+
+
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str = "5";
+let str2 = "five";
+let isPresent = false;
+
+
+
+
+
+console.log ("val"=="str");   true
+console.log ("val"==="str");   false
+console.log(!isPresent);       true
+console.log("eleven"== "str"&&"val">="str");  false 
+console.log(!isPresent || isPresent);    true 
+console.log(0 == false);  true
+console.log (0 === false);  false
+console.log (0 != false);   false
+console.log(0 !== false);  true 
+
+
+
